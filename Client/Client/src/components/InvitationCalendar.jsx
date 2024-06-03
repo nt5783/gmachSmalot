e75f4ee73a98752f8dd2d7d0f54be6cb33eca1a8
feigiz <pz0933@gmail.com>
2024-06-03 18:20:51 +0300

    C: define the calendar

--- Client/Client/src/components/InvitationCalendar.jsx
+++ Client/Client/src/components/InvitationCalendar.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useForm } from "react-hook-form";
 // import { AppContext } from "../App";
@@ -6,24 +6,26 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
 function InvitationCalendar() {
-    const navigate = useNavigate();
-    const [value, setValue] = useState(new Date());
+  const navigate = useNavigate();
+  const [value, setValue] = useState(new Date());
 
-    function onChange(nextValue) {
-      setValue(nextValue);
-    }
-    
-    return (
-        <div style={{margin:"300px"}}>
-                <Calendar calendarType='hebrew'
-                onClickDay={(value)=>navigate('/gowns', { state: { value } })}
-                // onClickDay={(value, event) => alert('Clicked day: ', value)}
-        onChange={onChange}
+  function onChange(nextValue) {
+    setValue(nextValue);
+  }
+
+  var oneYearFromNow = new Date();
+  oneYearFromNow.setFullYear(oneYearFromNow.getFullYear() + 1);
+  //שיוכלו לבחור מהיום או ממחרתיים?
+  return (
+    <div style={{ margin: "300px" }}>
+      <Calendar maxDate={oneYearFromNow} minDate={new Date()} minDetail='year' calendarType='hebrew' next2Label={null} prev2Label={null}
+        onClickDay={(value) => navigate('/gowns', { state: { value } })}
+        onChange={onChange} tileDisabled={({ activeStartDate, date, view }) => date.getDay() === 6 || date.getDay() === 5}
         value={value}
-      />  
-        </div>
+      />
+    </div>
 
-    );
+  );
 }
 export default InvitationCalendar;
 
