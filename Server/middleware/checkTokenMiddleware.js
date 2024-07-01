0b5f7b95c0ffade95e159d2ffb62637acd7859bb
nt5783 <nt5783@mailbox.org>
2024-07-01 21:44:35 +0300

    S: add middleware for token verify

--- Server/middleware/checkTokenMiddleware.js
+++ Server/middleware/checkTokenMiddleware.js
@@ -1,21 +1,15 @@
 import jwt from 'jsonwebtoken'
 
-export function checkTokenManager(req, res, next) {
+export default function checkToken(req, res, next) {
     const authorizationHeader = req.header('Authorization')
     const token = authorizationHeader.split(' ')[1].split('=')[1]
     if (!token) {
         return res.status(401).json({ message: 'Authorization token is missing' })
     }
     try {
-        const decodedToken = jwt.verify(token, process.env.RANDOM_TOKEN_SECRET)
-        const isAdmin = decodedToken.admin;
-        console.log('isAdmin')
-        console.log(isAdmin)
-        if (!isAdmin) return res.status(401).json({ message: 'Unauthorized' })
+        jwt.verify(token, process.env.RANDOM_TOKEN_SECRET)
         next()
     } catch (error) {
         return res.status(401).json({ message: 'Invalid token' })
     }
 }
-
-export default checkTokenManager
--- Server/router/gownRouter.js
+++ Server/router/gownRouter.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {GownController} from "../controllers/gownController.js";
+import {checkTokenManager} from '../middleware/authMiddleware.js'
 
 const gownRouter = express.Router();
 
@@ -7,7 +8,7 @@ const gownController = new GownController();
 
 gownRouter.get("/:id", gownController.getGownById)
 gownRouter.get("/", gownController.getGowns)
-gownRouter.post("/", gownController.addGown)
+gownRouter.post("/", checkTokenManager, gownController.addGown)
 gownRouter.patch("/:id", gownController.updateGown)
 gownRouter.delete("/:id", gownController.deleteGown)
 
