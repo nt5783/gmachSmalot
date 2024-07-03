ab0106a04a802e7894974b866b432b77880251e9
nt5783 <nt5783@mailbox.org>
2024-07-03 16:13:18 +0300

    S: verify token, log result

--- Server/middleware/verifyToken.js
+++ Server/middleware/verifyToken.js
@@ -1,24 +1,17 @@
 import jwt from 'jsonwebtoken'
 
 export function verifyToken(req, res, next) {
-    // const authorizationHeader = req.header('Authorization')
-    // console.log('req')
-    // console.log(req.headers)
-    // console.log('authorizationHeader')
-    // console.log(authorizationHeader)
-    // const tokenParts = authorizationHeader.split(' ')
-    // const tokenVar = tokenParts[1]
-    // const token = tokenVar.split('=')[1]
-    // // const token = authorizationHeader.split(' ')[1].split('=')[1]
-    // console.log('token')
-    // console.log(token)
-    // if (!token) {
-    //     return res.status(401).json({ message: 'Authorization token is missing' })
-    // }
+    const authorizationHeader = req.header('Authorization')
+    const token = authorizationHeader.split(' ')[1].split('=')[1]
+    if (!token) {
+        return res.status(401).json({ message: 'Authorization token is missing' })
+    }
     try {
-        // jwt.verify(token, process.env.RANDOM_TOKEN_SECRET)
+        jwt.verify(token, process.env.RANDOM_TOKEN_SECRET)
+        console.log('verified!!')
         next()
     } catch (error) {
+        console.log('not verified:(')
         return res.status(401).json({ message: 'Invalid token' })
     }
 }
