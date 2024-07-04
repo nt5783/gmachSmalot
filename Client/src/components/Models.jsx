5fc03e42c3e31b80b749944e53c321f49d710f52
feigiz <pz0933@gmail.com>
2024-07-04 18:20:26 +0300

    C: finish to hendal delete model

--- Client/src/components/Models.jsx
+++ Client/src/components/Models.jsx
@@ -22,8 +22,8 @@ function Models() {
     const { date, setDate } = useContext(DateContext);
     const [models, setModels] = useState([]);
     const [addModelForm, setAddModelForm] = useState(false);
-    const newDate = date? new Date(date): null;
-    const [ eventDate, setEventDate] = useState(newDate);
+    const newDate = date ? new Date(date) : null;
+    const [eventDate, setEventDate] = useState(newDate);
 
     async function getData() {
         const res = date ? fetchNoParamsfunc(`models?date=${eventDate}`, 'GET') : fetchNoParamsfunc(`models`, 'GET');
@@ -47,6 +47,9 @@ function Models() {
             try {
                 //לבדוק שמשפיע בצורה נכונה
                 fetchNoParamsfunc(`models/${model}`, 'DELETE')
+                const index = models.findIndex(m => m.model === model);
+                setModels((prev) => [...prev.slice(0, index), ...prev.slice(index + 1, prev.length)])
+                alert(`model ${model} deleted successfully`)
             } catch (e) { alert(e) }
         }
     }
@@ -55,30 +58,37 @@ function Models() {
         return (
             // onMouseOver={}
             <Card className='model_item' onClick={() => navigate(`./${model.model}`, { state: { model: model, eventDate: eventDate } })}>
-                {user && user.isManager === 1 && <img onClick={() => deleteModel(model.model)} src={trash} />}
+                {user && user.isManager === 1 && <img onClick={(event) => { event.stopPropagation(); deleteModel(model.model) }} src={trash} />}
                 <h3>{model.model}</h3>
                 <img height={400} src={model.image} alt={model.model} />
             </Card>
         );
     };
 
+    function modelsHeader() {
+        return (
+            <div className='models-header'>
+                <span>Models </span>
+                {user && user.isManager === 1 && (
+                    <Button label="Add New Model" icon="pi pi-plus" onClick={() => setAddModelForm(prev => !prev)} />)}
+            </div>
+        )
+    }
 
     return (
         <div className="models-page">
-            {user && user.isManager === 1 && (
-                <Button label="Add New Model" icon="pi pi-plus" onClick={() => setAddModelForm(prev => !prev)} />
-            )}
             {addModelForm && <AddModel formOn={setAddModelForm} />}
-            <Panel header="Models" className="models-panel">
+            <Panel header={modelsHeader}
+            className="models-panel">
                 {date != null ? (
                     <div className="event-info">
                         <p>The models with gowns available for your event: <b>{eventDate.getDate()}/{eventDate.getMonth() + 1}/{eventDate.getFullYear()}. </b>
-                        <a className="no-background" href="./eventCalendar">Change date</a> or <a href="#" onClick={() => handleClearDate()}> clear date</a></p>
+                            <a className="no-background" href="./eventCalendar">Change date</a> or <a href="#" onClick={() => handleClearDate()}> clear date</a></p>
                     </div>
                 ) : (
                     <>
                         <div className="warning">Pay attention that the gown may not be available for the date of your event.
-                        <br />To view only models with gowns available for the date of your event <a className="no-background" href="./eventCalendar">pick a date here</a></div>
+                            <br />To view only models with gowns available for the date of your event <a className="no-background" href="./eventCalendar">pick a date here</a></div>
                     </>
                 )}
                 {models.length > 0 && <div className="filter-by"></div>}
@@ -89,4 +99,3 @@ function Models() {
 }
 
 export default Models;
-
