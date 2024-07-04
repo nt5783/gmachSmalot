d4b7c8f843fee8277293c19cf742c6dbf97f0459
feigiz <pz0933@gmail.com>
2024-07-04 12:55:31 +0300

    S: start to handel errors

--- Server/service/db.js
+++ Server/service/db.js
@@ -16,6 +16,7 @@ async function executeQuery(query, params) {
         [results] = await connection.execute(query, params);
     } catch (err) {
         console.log(err);
+        throw err;
     }
     finally {
         connection.end();
