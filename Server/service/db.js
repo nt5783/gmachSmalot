76de56850c00eea21fbbd7c4c2af5529bbec7a47
nt5783 <nt5783@mailbox.org>
2024-07-04 14:56:29 +0300

    S: adaptable db function for our different computers

--- Server/service/db.js
+++ Server/service/db.js
@@ -4,14 +4,17 @@ import 'dotenv/config'
 
 async function executeQuery(query, params) {
     let results;
-    const connection = await mysql.createConnection({
+    const connection = process.env.DB_TYPE == "mariadb" ? await createConnection({
+        
+    })
+    : await mysql.createConnection({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         // socketPath: process.env.DB_SOCKET,
         //  port: process.env.DB_PORT,
         database: process.env.DB_NAME,
         password: process.env.DB_PASSWORD
-    });
+    })
     try {
         [results] = await connection.execute(query, params);
     } catch (err) {
