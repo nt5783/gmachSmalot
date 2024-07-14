3c4c2c31c79793c30a0f4ce2c33384bea5966fcf
feigiz <pz0933@gmail.com>
2024-07-14 21:13:50 +0300

    S: generic properties query

--- Server/service/lengthService.js
+++ Server/service/lengthService.js
@@ -1,40 +1,23 @@
 
 import { executeQuery } from './db.js';
-import { getLengthsQuery, getLengthByIdQuery, addLengthQuery, deleteLengthQuery, updateLengthQuery } from '../queries/lengthQuery.js'
+import { getPropertiesQuery, getPropertyByIdQuery, addPropertyQuery } from '../queries/propertyQuery.js'
 
 export class LengthService {
-
     async getLengths(queryparams) {
-        const queryLength = getLengthsQuery(queryparams);
+        const queryLength = getPropertiesQuery(queryparams,'length');
         const result = await executeQuery(queryLength);
         return result;
     }
 
     async getLengthById(id) {
-        const queryLength = getLengthByIdQuery();
+        const queryLength = getPropertyByIdQuery('length');
         const result = await executeQuery(queryLength, [id]);
-        // console.log(result)
         return result;
     }
 
     async addLength(newLength) {
-        console.log(newLength)
-        const queryLength = addLengthQuery();
+        const queryLength = addPropertyQuery('length');
         const result = await executeQuery(queryLength, Object.values(newLength));
-        return result;
-    }
-
-    async deleteLength(id) {
-        const queryLength = deleteLengthQuery();
-        const result = await executeQuery(queryLength, [id]);
-        return result;
-    }
-
-    async updateLength(updatedLength, id) {
-        let data = Object.values(updatedLength);
-        data.push(id)
-        const queryLength = updateLengthQuery(Object.keys(updatedLength));
-        const result = await executeQuery(queryLength, data);
         return result;
     }
 }
--- Server/service/sizeService.js
+++ Server/service/sizeService.js
@@ -1,40 +1,24 @@
 
 import { executeQuery } from './db.js';
-import { getSizesQuery, getSizeByIdQuery, addSizeQuery, deleteSizeQuery, updateSizeQuery } from '../queries/sizeQuery.js'
+import { getPropertiesQuery, getPropertyByIdQuery, addPropertyQuery } from '../queries/propertyQuery.js'
+
 
 export class SizeService {
-
     async getSizes(queryparams) {
-        const querySize = getSizesQuery(queryparams);
+        const querySize = getPropertiesQuery(queryparams,'size');
         const result = await executeQuery(querySize);
         return result;
     }
 
     async getSizeById(id) {
-        const querySize = getSizeByIdQuery();
+        const querySize = getPropertyByIdQuery('size');
         const result = await executeQuery(querySize, [id]);
-        // console.log(result)
         return result;
     }
 
     async addSize(newSize) {
-        console.log(newSize)
-        const querySize = addSizeQuery();
+        const querySize = addPropertyQuery('size');
         const result = await executeQuery(querySize, Object.values(newSize));
-        return result;
-    }
-
-    async deleteSize(id) {
-        const querySize = deleteSizeQuery();
-        const result =  await executeQuery(querySize, [id]);
-        return result;
-    }
-
-    async updateSize(updatedSize, id) {
-        let data = Object.values(updatedSize);
-        data.push(id)
-        const querySize = updateSizeQuery(Object.keys(updatedSize));
-        const result = await executeQuery(querySize, data);
         return result;
     }
 }
